e454cddc1c01e35eaa932faaa77befb8f498aff4
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-03-18 19:38:02 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Tushar Munge on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
